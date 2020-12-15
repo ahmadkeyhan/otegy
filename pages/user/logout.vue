8a469b9171eba66b2afdf00a425d6e6b00276897
ahmadkeyhan <keyhan.ahmad@gmail.com>
2020-12-15 16:18:11 +0330

    added logout and panel pages

---
+++ pages/user/logout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>خروج از حساب کاربری</h1>
+    <hr>
+
+    <p>لطفا کمی صبر کنید...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: 'auth',
+
+  async asyncData(context){
+    await context.$auth.logout()
+  }
+}
+</script>
---
+++ pages/user/panel.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>پنل کاربری</h1>
+    <hr>
+
+    <div class="card">
+      <div class="card-body">
+        <h4>{{ this.$auth.user.username }}</h4>
+        <div class="mb-5"><strong>Email:</strong> {{ this.$auth.user.email }}</div>
+        <nuxt-link to="/user/logout" class="btn btn-danger">خروج از حساب کاربری</nuxt-link>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: 'auth'
+}
+</script>
