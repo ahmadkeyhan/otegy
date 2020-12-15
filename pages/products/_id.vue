1780cb4249c360c0bbdb5c51f06df3a9160c26fc
ahmadkeyhan <keyhan.ahmad@gmail.com>
2020-12-15 19:45:47 +0330

    added product pages which don't need auth

---
+++ pages/products/_id.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col">
+        <b-card :title="product.title" :sub-title="product.project" :img-src="product.url" :img-alt="product.title" img-top class="mb-2">
+          <b-card-text>
+            {{ product.body }}
+          </b-card-text>
+        </b-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData(context){
+    const {data} = await context.$axios.get('/api/products/' + context.route.params.id)
+    return {
+      article : data
+    }
+  }
+}
+</script>
---
+++ pages/products/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>محصولات</h1>
+    <hr>
+
+    <div class="row">
+      <div class="col-md-6">
+        <nuxt-link to="/products/posters">
+          پوستر ها
+        </nuxt-link>
+      </div>
+      <div class="col-md-6">
+        <nuxt-link to="/products/photos">
+          عکاسی ها
+        </nuxt-link>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
