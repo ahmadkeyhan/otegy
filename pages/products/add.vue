3a6b670d8f417698178fa2add7375bc6e1fb06cf
ahmadkeyhan <keyhan.ahmad@gmail.com>
2020-12-16 08:34:34 +0330

    added product creation form

---
+++ pages/products/add.vue
@@ -0,0 +1,117 @@
+<template>
+  <div>
+    <h1>اضافه کردن محصول</h1>
+    <hr>
+    <b-form action="" method="post" @submit.prevent="submitForm()">
+      <b-form-group
+        id="input-group-1"
+        label="عنوان"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="title"
+          type="text"
+          required
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group
+        id="input-group-2"
+        label="لینک"
+        label-for="input-2"
+      >
+        <b-form-input
+          id="input-2"
+          v-model="url"
+          type="text"
+          required
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group
+        id="input-group-3"
+        label="دسته بندی"
+        label-for="input-3"
+      >
+        <b-form-select
+          id="input-3"
+          v-model="type"
+          :options="types"
+          required
+        ></b-form-select>
+      </b-form-group>
+      <b-form-group
+        id="input-group-4"
+        label="پروژه"
+        label-for="input-4"
+      >
+        <b-form-select
+          id="input-4"
+          v-model="project"
+          :options="projects"
+          required
+        ></b-form-select>
+      </b-form-group>
+      <b-form-group
+        id="input-group-5"
+        label="توضیحات"
+        label-for="input-5"
+      >
+        <b-form-textarea
+          id="input-5"
+          v-model="body"
+          rows="3"
+          max-rows="5"
+          required
+        ></b-form-textarea>
+      </b-form-group>
+      <div class="alert alert-danger" v-if="error">{{ error }}</div>
+      <b-button type="submit" variant="primary">ثبت</b-button>
+      <b-button to="/user/panel" variant="danger">لغو</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: 'auth',
+  data () {
+    return {
+      error: null,
+      title: null,
+      url: null,
+      type: null,
+      project: null,
+      body: null,
+      types: [
+        { text: 'انتخاب کنید', value: null },
+        { text: 'عکس', value: 'photo' },
+        { text: 'ویدیو', value: 'video' },
+        { text: 'پوستر', value: 'poster' }
+      ],
+      projects: [
+        { text: 'انتخاب کنید', value: null },
+        'lee1'
+      ],
+      status: false
+    }
+  },
+
+  methods: {
+    submitForm () {
+      this.$axios.post('api/products/', {
+        title: this.title,
+        url: this.url,
+        type: this.type,
+        project: this.project,
+        body: this.body
+      })
+        .catch((error) => {
+          console.log(error)
+          if (error.response.data.message) {
+            this.login_error = error.response.data.message
+          }
+        })
+    }
+  }
+}
+</script>
--- pages/user/panel.vue
+++ pages/user/panel.vue
@@ -2,15 +2,17 @@
   <div>
     <h1>پنل کاربری</h1>
     <hr>
-
-    <div class="card">
-      <div class="card-body">
-        <h4>{{ this.$auth.user.username }}</h4>
-        <div class="mb-5"><strong>Email:</strong> {{ this.$auth.user.email }}</div>
-        <nuxt-link to="/user/logout" class="btn btn-danger">خروج از حساب کاربری</nuxt-link>
-      </div>
-    </div>
-
+    <b-card
+      :title="this.$auth.user.username"
+      :sub-title="this.$auth.user.email"
+    >
+      <b-card-text>
+        از این قسمت میتوانید محصول اضافه یا حذف کنید
+      </b-card-text>
+      <b-link href="/products/add" class="card-link">اضافه کردن محصول</b-link>
+      <b-link href="/products" class="card-link">حذف محصول</b-link>
+    </b-card>
+    <nuxt-link to="/user/logout" class="btn btn-danger">خروج از حساب کاربری</nuxt-link>
   </div>
 </template>
 
