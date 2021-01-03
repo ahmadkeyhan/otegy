87496c982e2095a2da981c5b68d41db71f0ff135
MedOlica <keyhan.ahmad@gmail.com>
2021-01-03 17:00:36 +0330

    hid the mongodb connection
    tnx to gitGuardian email

--- api/db.js
+++ api/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // mongodb database connection string. change it as per your needs. here "mydb" is the name of the database. You don't need to create DB from mongodb terminal. mongoose create the db automatically.
-mongoose.connect('mongodb://mocubeir_user:M1234612m@mocube.ir:27017/mocubeir_db', {
+mongoose.connect('mongodb://', {
                             useNewUrlParser: true,
                             useUnifiedTopology: true,
                             useFindAndModify: false,
